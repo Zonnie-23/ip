763bdead77e0f0aefec1a970f62d2fb87c99d426
Zonnie-23 <cyenzot@gmail.com>
2025-01-22 13:26:26 +0800

    Renamed to Nova and added greetings

---
+++ src/main/java/Nova.java
@@ -0,0 +1,12 @@
+public class Nova {
+    public static void main(String[] args) {
+        String lineBreak = "____________________________________________________________\n";
+
+        System.out.println(lineBreak
+                + "Hello! I'm Nova.\n"
+                + "What can I do for you?\n"
+                + lineBreak
+                + "Bye. Hope to see you again soon!\n"
+                + lineBreak);
+    }
+}
